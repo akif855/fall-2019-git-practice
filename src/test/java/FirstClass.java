19191f9cce2cbb529fc5ad9b660545e736cf472b
AkifValiyev <akifvaliyev1973@gmail.com>
2020-03-25 19:01:49 -0400

    change

--- src/test/java/FirstClass.java
+++ src/test/java/FirstClass.java
@@ -2,5 +2,9 @@ public class FirstClass {
     public static void main(String[] args) {
         System.out.println("brave new world");
         System.out.println("someone");
+        System.out.println("change one");
+        System.out.println("change two");
+        System.out.println("change three");
+
     }
 }
